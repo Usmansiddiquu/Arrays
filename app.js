523009b4407c881d068c6db2de6c6136f327647d
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-03-04 16:49:59 +0500

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 
+// var studentsnames = [ ]
+
+// var names = ["names"]
+
+// var studentsnames = [2,3,4,5]
+// console.log(studentsnames[3])
+
+// var boolArray = [true, false, true, false];
+// console.log(boolArray)
+
+// var mixedArray = [1, "Hello", true, 3.14];
+// console.log(mixedArray)
+
+// var eduqual = ["1)SSC","HSC","BS","BCOM","MS","M.Phil","PhD"];
+// document.write(eduqual);
+
+// var studentsnames =['usman','umar','raza']
+// var scores =['320','230','480']
+
+// var total1 =(scores[0]/500*100)
+// var total2 =(scores[1]/500*100)
+// var total3 =(scores[2]/500*100)
+
+// var finalscores1 = ["score of usman is " + scores[0] + " and the percentage is " + total1 ]
+// var finalscores2 = ["score of umar is " +  scores[1] +" and the percentage is " + total2 ]
+// var finalscores3 = ["score of raza is " +  scores[2] + " and the percentage is " + total3 ]
+
+// var grandfinal =[finalscores1,finalscores2,finalscores3]
+// console.log(grandfinal)
+
+// var colornames =["red","green","blue"]
+
+// console.log(colornames)
+
+// var addcolor =prompt("what color you want to add")
+// var colour = ("addcolor" +"colour" )
